f7d2537a47055a775903663e1820a5da306c18e3
Artan Wellis <elias78210m@gmail.com>
2023-04-11 15:24:34 +0200

    debut du projet

---
+++ PiouPiou/app/src/main/java/com/example/pioupioy/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pioupioy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn2).setOnClickListener( clic -> {
+            Intent intent=new Intent(getApplicationContext(),inscrire.class);
+            System.out.println(intent);
+            startActivity(intent);
+        });
+
+        findViewById(R.id.label3).setOnClickListener( clic -> {
+            Intent intent=new Intent(getApplicationContext(),oublimdp.class);
+            startActivity(intent);
+        });
+    }
+}
