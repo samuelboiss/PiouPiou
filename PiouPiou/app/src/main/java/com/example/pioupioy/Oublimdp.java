c912549625c72d3b70a49be0dade29619bf37668
arnaud dumanois <arnaud.dumanois@etu.unice.fr>
2023-05-04 08:38:23 +0200

    feat: add the forget password with firebase ref #15

--- PiouPiou/app/src/main/java/com/example/pioupioy/Oublimdp.java
+++ PiouPiou/app/src/main/java/com/example/pioupioy/Oublimdp.java
@@ -2,7 +2,9 @@ package com.example.pioupioy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,18 +13,37 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Oublimdp extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private EditText mPassword;
-    private EditText mPasswordConfirm;
+    private EditText mEmailField;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.oublimdp);
-        findViewById(R.id.btn).setOnClickListener( clic -> {
-            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
-            System.out.println(intent);
-            startActivity(intent);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmailField = findViewById(R.id.textbox);
+
+        findViewById(R.id.btn).setOnClickListener( new View.OnClickListener(){
+            public void onClick(View view) {
+                String email = mEmailField.getText().toString();
+                if (email.isEmpty()) {
+                    Toast.makeText(Oublimdp.this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
+                } else {
+                    mAuth.sendPasswordResetEmail(email)
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Oublimdp.this, "Un Email de reinitialisation vous a été envoyé", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(Oublimdp.this, "L'Email n'existe pas", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            }
         });
     }
+
 }
