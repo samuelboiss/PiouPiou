906acbcac074267671277376c62d1b4c34773a52
arnaud dumanois <arnaud.dumanois@etu.unice.fr>
2023-05-07 22:59:51 +0200

    fix: XML is working good

--- PiouPiou/app/src/main/java/com/example/pioupioy/Recensement.java
+++ PiouPiou/app/src/main/java/com/example/pioupioy/Recensement.java
@@ -1,6 +1,9 @@
 package com.example.pioupioy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
