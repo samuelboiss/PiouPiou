2b8a2c3d66f55685704db79698b0f503f4edb129
arnaud dumanois <arnaud.dumanois@etu.unice.fr>
2023-05-07 18:42:10 +0200

    feat: create XML for the census edit 2

--- PiouPiou/app/src/main/java/com/example/pioupioy/Recensement.java
+++ PiouPiou/app/src/main/java/com/example/pioupioy/Recensement.java
@@ -1,2 +1,14 @@
-package com.example.pioupioy;public class Recensement {
+package com.example.pioupioy;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Recensement extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recensement);
+    }
 }
