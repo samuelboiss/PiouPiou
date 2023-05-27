3a26899612578c7df05e052ecc4006ac49f7f300
Orfila Marie <marie.orfila@etu.univ-cotedazur.fr>
2023-05-27 22:35:09 +0200

    refactor: modify bird event classes

--- PiouPiou/app/src/main/java/com/example/pioupioy/BirdObservation.java
+++ PiouPiou/app/src/main/java/com/example/pioupioy/BirdObservation.java
@@ -7,34 +7,13 @@ import androidx.annotation.NonNull;
 
 import org.osmdroid.util.GeoPoint;
 
-public class BirdCensus extends BirdEvent implements Parcelable {
+public class BirdObservation extends BirdEvent implements Parcelable {
     private BirdEventType type;
 
-    public BirdCensus(String name, int numberOfBird, String  date, byte[] image, boolean huntable, GeoPoint address, String direction, String meteo) {
+    public BirdObservation(String name, int numberOfBird, String date, byte[] image, boolean huntable, GeoPoint address, String direction, String meteo) {
         super(name, numberOfBird, date, image, huntable, address, direction, meteo);
-        this.type = BirdEventType.CENSUS;
+        this.type = BirdEventType.OBSERVATION;
     }
-
-    protected BirdCensus(Parcel in) throws IllegalAccessException {
-        super(in.readString(), in.readInt(), in.readString(), in.createByteArray(), in.readByte() != 0, new GeoPoint(in.readDouble(), in.readDouble()), in.readString(), in.readString());
-        this.type = BirdEventType.CENSUS;
-    }
-
-    public static final Creator<BirdCensus> CREATOR = new Creator<BirdCensus>() {
-        @Override
-        public BirdCensus createFromParcel(Parcel in) {
-            try {
-                return new BirdCensus(in);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        @Override
-        public BirdCensus[] newArray(int size) {
-            return new BirdCensus[size];
-        }
-    };
 
     @Override
     public String getLabel() {
@@ -44,6 +23,28 @@ public class BirdCensus extends BirdEvent implements Parcelable {
     public BirdEventType getType() {
         return this.type;
     }
+
+    protected BirdObservation(Parcel in) throws IllegalAccessException {
+        super(in.readString(), in.readInt(), in.readString(), in.createByteArray(), in.readByte() != 0, new GeoPoint(in.readDouble(), in.readDouble()), in.readString(), in.readString());
+        this.type = BirdEventType.OBSERVATION;
+    }
+
+    public static final Creator<BirdObservation> CREATOR = new Creator<BirdObservation>() {
+        @Override
+        public BirdObservation createFromParcel(Parcel in) {
+            try {
+                return new BirdObservation(in);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public BirdObservation[] newArray(int size) {
+            return new BirdObservation[size];
+        }
+    };
+
 
     @Override
     public int describeContents() {
@@ -61,4 +62,6 @@ public class BirdCensus extends BirdEvent implements Parcelable {
         parcel.writeString(direction);
         parcel.writeString(weather);
     }
+
+
 }
---
+++ PiouPiou/app/src/main/java/com/example/pioupioy/utils/ImageUtils.java
@@ -0,0 +1,30 @@
+package com.example.pioupioy.utils;
+
+import android.graphics.Bitmap;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageUtils {
+
+    public static byte[] convert(String base64Str) throws IllegalArgumentException {
+        byte[] decodedBytes = Base64.decode(
+                base64Str.substring(base64Str.indexOf(",") + 1),
+                Base64.DEFAULT
+        );
+
+        return decodedBytes;
+    }
+
+    public static String convertFromByteArray(byte[] array) {
+        return Base64.encodeToString(array, Base64.DEFAULT);
+    }
+
+    public static byte[] convertToByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+
+        return outputStream.toByteArray();
+    }
+
+}
